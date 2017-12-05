55d561ad047d2123d25757c8119545e796149669
Etienne FICHOT <efichot@e2r4p4.42.fr>
2017-12-05 12:13:03 +0100

    try

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const routes = require('./routes');
+const router = require('./routes');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
-  .use('/', routes);
+  .use('/', router);
   
 
 

3cf6c6e8bc976cb468644eafac11e2f27994a5e6
Etienne FICHOT <efichot@e2r4p4.42.fr>
2017-12-05 12:11:56 +0100

    separate file routes

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-const router = express.Router();
+const routes = require('./routes');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
-
-  router.get('/', (req, res) => res.render('pages/index'))
-  router.post('/sendtoken', (req, res) => {
-    const { token } = req.body;
-    console.log("Token --> " + token);
-  })
+  .use('/', routes);
+  
 
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

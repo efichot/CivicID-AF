165c5631088cb861ea7fa5c93e052bbac9f7618b
Etienne FICHOT <efichot@e2r4p4.42.fr>
2017-12-05 12:20:04 +0100

    try app

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express();
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
 
-  .get('/', (req, res) => res.render('pages/index'))
-  .post('/sendtoken', (req, res) => {
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.post('/sendtoken', (req, res) => {
     const { token } = req.body;
     console.log("Token --> " + token);
   })

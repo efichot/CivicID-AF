0f0495ef0c0bdc5ac06803b57179caad85176841
Etienne FICHOT <efichot@e2r4p4.42.fr>
2017-12-05 12:42:59 +0100

    test

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ const app = express();
 
   app.get('/', (req, res) => res.render('pages/index'));
   app.post('/', (req, res) => res.send('post rq  from homepage!'))
-  app.post('/sendtoken', (req, res) => {
-    const { token } = req.body;
-    console.log("Token --> " + token);
-    res.send('token received!' + token);
-  });
+  app.post('/sendtoken', (req, res) =>
+    //const { token } = req.body;
+    //console.log("Token --> " + token);
+    res.send('token received!' + req.body.token));
 
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

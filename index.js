7b637e9966350612465ff0bd78ed4e71fa387b3d
Etienne FICHOT <efichot@e2r4p4.42.fr>
2017-12-05 12:30:23 +0100

    app fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express();
+  app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')

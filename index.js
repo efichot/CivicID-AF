979e1ae4c8f227f2626db08e87d5ec03ceee0ed4
Etienne FICHOT <efichot@e2r4p4.42.fr>
2017-12-05 12:15:25 +0100

    try delete router

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const router = require('./routes');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
-  .use('/', router)
-  
+
+  .get('/', (req, res) => res.render('pages/index'))
+  .post('/sendtoken', (req, res) => {
+    const { token } = req.body;
+    console.log("Token --> " + token);
+  })
 
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

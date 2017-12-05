0f6d934211859f5b29c49ed9118416fb04fdeeec
Etienne FICHOT <efichot@e2r4p4.42.fr>
2017-12-05 12:03:56 +0100

    modif express() into const router

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const router = express.router();
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
 
-  .get('/', (req, res) => res.render('pages/index'))
-  .post('/sendtoken', (req, res) => {
+  router.get('/', (req, res) => res.render('pages/index'))
+  router.post('/sendtoken', (req, res) => {
     const { token } = req.body;
     console.log("Token --> " + token);
   })
